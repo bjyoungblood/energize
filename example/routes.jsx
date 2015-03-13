2ef5f4f382e69f0f8795597178996b3e2c49de61
Ben Youngblood <bx.youngblood@gmail.com>
2015-03-12 17:02:59 -0700

    necessary to include react (so it is available for transformed code)

--- example/routes.jsx
+++ example/routes.jsx
@@ -1,6 +1,7 @@
 /*eslint no-unused-vars:0,no-undef:0*/
 // https://github.com/eslint/eslint/issues/1978
 
+import React from 'react';
 import { Route } from 'react-router';
 
 import AppLayout from './layouts/app';
