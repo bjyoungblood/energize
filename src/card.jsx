58a1de7a070d8dca53daeb724705b54562cfc456
Isaiah DeRose-Wilson <isaiahdw@users.noreply.github.com>
2015-03-12 19:46:56 -0700

    Add card component

---
+++ src/card.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Card = React.createClass({
+  propTypes : {
+    title : React.PropTypes.string,
+    theme : React.PropTypes.string,
+    contentTheme : React.PropTypes.string,
+  },
+
+  getDefaultProps() {
+    return {
+      theme : 'blue-grey darken-1',
+      contentTheme : 'white-text',
+    };
+  },
+
+  renderTitle() {
+    if ( ! this.props.title) {
+      return null;
+    }
+
+    return (
+      <span className="card-title">
+        {this.props.title}
+      </span>
+    );
+  },
+
+  render() {
+
+    return (
+      <div className={'card ' + this.props.theme}>
+        <div className={'card-content ' + this.props.contentTheme}>
+          {this.renderTitle()}
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+});
+
+export default Card;
